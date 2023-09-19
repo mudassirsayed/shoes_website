ad05fdbd7f90d9471127947ea2644ba68bdc41aa
@mudassir2 <mudassir@nimapinfotech.com>
2023-09-19 22:10:04 +0530

    added tailwindcss config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-import './App.css'
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  )
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
-
-export default App
